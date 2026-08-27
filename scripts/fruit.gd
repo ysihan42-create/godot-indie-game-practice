3596e14e990b8a11c67cc801ec430723e22dbdf7
ysihan42-create <ysihan42@gmail.com>
2026-08-27 20:52:08 +0800

    Use 12 fruit variants in fruit item

--- scripts/fruit.gd
+++ scripts/fruit.gd
@@ -1,10 +1,15 @@
 extends Area2D
 
 
+@export var fruit_type := 0
 @onready var game_manager: Node = %GameManager
+@onready var sprite: Sprite2D = $Sprite2D
 
 
 func _ready() -> void:
+	var col := fruit_type % 3
+	var row := int(fruit_type / 3.0)
+	sprite.region_rect = Rect2(col * 16, row * 16, 16, 16)
 	body_entered.connect(_on_body_entered)
 
 
