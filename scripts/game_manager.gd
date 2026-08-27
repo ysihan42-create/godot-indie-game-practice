a82e11fd9dd8853f3a04d05fc10fca47d3f44297
ysihan42-create <ysihan42@gmail.com>
2026-08-27 22:30:13 +0800

    Move death respawn logic to persistent GameManager

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,22 @@
 extends Node
 
 var score = 0
+var is_dying := false
 
 @onready var score_label: Label = $ScoreLabel
 
 func add_point(amount: int = 1):
 	score += amount
 	score_label.text = "Score: " + str(score)
+
+
+func player_died(body: Node2D) -> void:
+	if is_dying:
+		return
+	is_dying = true
+	Engine.time_scale = 0.5
+	if body.has_node("CollisionShape2D"):
+		body.get_node("CollisionShape2D").queue_free()
+	await get_tree().create_timer(0.6).timeout
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene()
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,16 +1,13 @@
 extends Area2D
 
 
-@onready var timer: Timer = $Timer
-
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("is_invincible_now") and body.is_invincible_now():
 		return
-	print("You died!")
-	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
-	timer.start()
+	var game_manager := get_node_or_null("%GameManager")
+	if game_manager and game_manager.has_method("player_died"):
+		game_manager.player_died(body)
+
+
 func _on_timer_timeout() -> void:
-	Engine.time_scale = 1
-	get_tree().reload_current_scene()
+	pass
